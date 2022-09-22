b5d40e0b80a175fff3ca21a000ef3c01af75036a
jamalkitheka <jamalkitheka27@gmail.com>
2022-09-22 11:13:09 -0700

    0. Import

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def add_result():
+    a = 1
+    b = 2
+
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+
+
+if __name__ == "__main__":
+    from add_0 import add
+    add_result()
